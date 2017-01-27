cd4e96edf965dd2ab2422dd719a16bf5bb26ba68
Sebastian DiPirro <sebastiandipirro@Sebastians-MacBook-Air.local>
2017-01-26 18:33:53 -0800

    Initial Commit

---
+++ DevChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevChat
+//
+//  Created by Sebastian DiPirro on 1/26/17.
+//  Copyright © 2017 Sebastian DiPirro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
