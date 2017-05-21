4accc92442a4ee02548ec9675ca0c0c196f2ff5b
Sebastian DiPirro <sebastiandipirro@Sebastians-MacBook-Air.local>
2017-05-21 09:05:57 -0700

    Basic Sign in layout complete

--- DevChat/AppDelegate.swift
+++ DevChat/AppDelegate.swift
@@ -11,16 +11,22 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+ 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         
         FIRApp.configure()
         return true
     }
+    
+    
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+//        
+//        FIRApp.configure()
+//        
+//        return true
+//    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
