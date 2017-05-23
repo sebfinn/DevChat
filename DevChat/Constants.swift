6dc5fd1a2e9b5809c05378c7a27d6c34bb69d79f
Sebastian DiPirro <sebastiandipirro@Sebastians-MacBook-Air.local>
2017-05-22 22:18:34 -0700

    Sign in UI custom classes complete

---
+++ DevChat/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  DevChat
+//
+//  Created by Sebastian DiPirro on 5/22/17.
+//  Copyright © 2017 Sebastian DiPirro. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
