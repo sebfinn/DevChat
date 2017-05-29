1b31f175a35b96743fd17c3bb616a9f82388a454
Sebastian DiPirro <sebastiandipirro@Sebastians-MacBook-Air.local>
2017-05-29 14:25:55 -0700

    feed UI part 1 complet

---
+++ DevChat/CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  DevChat
+//
+//  Created by Sebastian DiPirro on 5/29/17.
+//  Copyright © 2017 Sebastian DiPirro. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
