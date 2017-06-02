7d0fee76e7b237566b730b505324a784ef4e7c4e
Sebastian DiPirro <sebastiandipirro@Sebastians-MacBook-Air.local>
2017-06-01 19:52:47 -0700

    Completed Firebase Display

--- DevChat/PostCell.swift
+++ DevChat/PostCell.swift
@@ -15,6 +15,8 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var postImg: UIImageView!
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
 
 
     override func awakeFromNib() {
@@ -22,7 +24,11 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
     
-    
-    
+    func configureCell(post: Post) {
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+        
+    }
 
 }
